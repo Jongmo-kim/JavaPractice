4c4e93b83295907448c163c5128cbea65d79604f
jongmo <wa1358@naver.com>
2020-07-20 21:40:59 +0900

    JLabel

--- tEST/tEST/ttest.java
+++ tEST/tEST/ttest.java
@@ -2,17 +2,12 @@ package tEST;
 /*
  * JLabel에 대한 기본적인 사용방법을 배우고 문자열안에 HTML 코드를 넣어서 활용할수도 있다.
  */
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.Point;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 class ttest{
 	public static void main(String[] args){
 		Myclass f = new Myclass();
-		Myclass f2= new Myclass();
-		Point p=f.getLocation();
-		
 		
 		}
 }
