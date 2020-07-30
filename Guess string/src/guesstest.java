517aa3a2189f27f9d91499b822e8281e8acef3b7
2.2 <2.2>
2020-07-30 17:18:17 +0900

    1차 연습문제 풀에

--- Guess string/src/guesstest.java
+++ Guess string/src/guesstest.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.lang.Character.*;
 public class guesstest {
 
 	public static void main(String[] args) {
@@ -14,7 +14,10 @@ public class guesstest {
 class MyFrame extends JFrame implements ActionListener{
 	JTextField tf;
 	String Word;
-	JLabel goal;
+	JLabel goal,p3_CurrentTimes;
+	
+	int CurrentCount,LimitCount;
+	
 	MyFrame(String s){
 		super(s);
 		setSize(400,250);
@@ -26,11 +29,9 @@ class MyFrame extends JFrame implements ActionListener{
 	private void makeUI() {
 		//word 부분
 		JPanel p1 = new JPanel();
-
-		Word="Programming";
+		Word="programming";
 		p1.add(new JLabel("Word : "));
 		p1.add(goal = new JLabel(Word));
-		
 		add(p1,BorderLayout.NORTH);
 		
 		//Guess 부분
@@ -46,36 +47,68 @@ class MyFrame extends JFrame implements ActionListener{
 		
 		//Progress 부분
 		JPanel p3 = new JPanel();
-		JLabel p3_l1,p3_CurrentTimes,p3_RemainTimes;
+		JLabel p3_l1,p3_LimitTimes;
+		CurrentCount = 0;
+		LimitCount = Word.length();
+		
 		p3.add(p3_l1=new JLabel("Progress :"));
-		p3.add(p3_CurrentTimes=new JLabel("0"));
-		p3.add(p3_RemainTimes=new JLabel("/0"));
+		p3.add(p3_CurrentTimes=new JLabel(Integer.toString(CurrentCount)));
+		p3.add(p3_LimitTimes=new JLabel("/" + Integer.toString(LimitCount)));
 		add(p3,BorderLayout.SOUTH);
 	}
 	public void actionPerformed(ActionEvent e) {
 		String TempToButtonString = tf.getText();
 		tf.setText("");
+		if(TempToButtonString.isEmpty() || TempToButtonString.isBlank()) {
+			JOptionPane.showMessageDialog(this, "잘못 입력하셨습니다.","Alert",JOptionPane.OK_OPTION);
+		} 
+		else {
 		if(CompareWord(TempToButtonString))
 			System.out.println("맞았습니다!");
 		else
 			System.out.println("틀렸습니다!");
+		if(isAllMatched())
+				JOptionPane.showMessageDialog(this, "모두 맞았습니다.", "Congrats", JOptionPane.OK_OPTION);
+		if(isLimitReached()){
+			JOptionPane.showMessageDialog(this, "남은기회가 없습니다.\n프로그램을 종료합니다.", "Alert", JOptionPane.OK_OPTION);
+			System.exit(0);
+		}
+		}
+		
 	}
 	private boolean CompareWord(String input) {
 		int count=0;
+
 		for (int List_i = 0; List_i < Word.length(); ++List_i) {
 			char des = input.charAt(0);
+			des=java.lang.Character.toLowerCase(des);
 			if (des ==Word.charAt(List_i)) {
 				Word = Word.replace(des, '*');
-				System.out.println(Word);
 				++count;
 			}
 		}
+		++CurrentCount;
+		p3_CurrentTimes.setText(Integer.toString(CurrentCount));
 			if (count>0){
-				goal= new JLabel(Word);
+				goal.setText(Word);;
 				return true;
 			}
 			return false;
 
 		}
+	private boolean isAllMatched() {
+		for(int List_i=0;List_i<Word.length();++List_i) {
+			if(Word.charAt(List_i)!='*') {
+				return false;
+			}
+			
+		}
+		return true;
+	}
+	private boolean isLimitReached() { 
+		if(CurrentCount<LimitCount)
+			return false;
+		return true;
+	}
 	
 }
